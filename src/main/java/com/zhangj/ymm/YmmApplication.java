7fe71b5afbb88cf806d0eca56c6e2f1ee2a54e3a
zhangj <814674562@qq.com>
2019-03-07 20:58:48 +0800

    Initial commit

---
+++ src/main/java/com/zhangj/ymm/YmmApplication.java
@@ -0,0 +1,13 @@
+package com.zhangj.ymm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmmApplication.class, args);
+    }
+
+}
