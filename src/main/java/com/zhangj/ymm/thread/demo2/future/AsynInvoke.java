158d7066309a445226120e18a03b95b14b6b76fe
zhangj <814674562@qq.com>
2019-03-12 23:16:56 +0800

    添加线程future设计模式

--- src/main/java/com/zhangj/ymm/thread/demo2/future/AsynInvoke.java
+++ src/main/java/com/zhangj/ymm/thread/demo2/future/AsynInvoke.java
@@ -3,9 +3,18 @@ package com.zhangj.ymm.thread.demo2.future;
 /**
  * @author zhangj
  * @date 2019/3/12
+ * Future  ->  代表未来的一个凭据
+ * FutureTask  -> 将自己的调用逻辑进行隔离
+ * FutureService -> 桥接Future和FutureTask的作用
  */
 public class AsynInvoke {
     public static void main(String[] args) throws InterruptedException {
+        futureMethod();
+        System.out.println("************************************");
+        callbackMethod();
+    }
+
+    private static void futureMethod() throws InterruptedException {
         FutureService futureService = new FutureService();
         Future<String> future = futureService.submit(() -> {
             try {
@@ -20,5 +29,21 @@ public class AsynInvoke {
         Thread.sleep(1000);
         System.out.println("===================================");
         System.out.println(future.get());
+    }
+
+    private static void callbackMethod() throws InterruptedException {
+        FutureService futureService = new FutureService();
+        futureService.submit(() -> {
+            try {
+                Thread.sleep(10_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "SUCCESS";
+        }, System.out::println);
+        System.out.println("===================================");
+        System.out.println("do other things......");
+        Thread.sleep(1000);
+        System.out.println("===================================");
     }
 }
--- src/main/java/com/zhangj/ymm/thread/demo2/future/FutureService.java
+++ src/main/java/com/zhangj/ymm/thread/demo2/future/FutureService.java
@@ -1,5 +1,7 @@
 package com.zhangj.ymm.thread.demo2.future;
 
+import java.util.function.Consumer;
+
 /**
  * @author zhangj
  * @date 2019/3/12
@@ -10,6 +12,16 @@ public class FutureService {
         new Thread(() -> {
             T result = task.call();
             asynFuture.done(result);
+        }).start();
+        return asynFuture;
+    }
+
+    public <T> Future<T> submit(final FutureTask<T> task, final Consumer<T> consumer) {
+        AsynFuture<T> asynFuture = new AsynFuture<>();
+        new Thread(() -> {
+            T result = task.call();
+            asynFuture.done(result);
+            consumer.accept(result);
         }).start();
         return asynFuture;
     }
