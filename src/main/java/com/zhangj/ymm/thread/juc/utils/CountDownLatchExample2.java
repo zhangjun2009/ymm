c11800ef1ce7597cca3926b34c4337f73022ab9d
zhangj <814674562@qq.com>
2019-04-09 09:20:42 +0800

    countdownlatch test

---
+++ src/main/java/com/zhangj/ymm/thread/juc/utils/CountDownLatchExample2.java
@@ -0,0 +1,42 @@
+package com.zhangj.ymm.thread.juc.utils;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author zhangj
+ * @date 2019/4/8
+ */
+public class CountDownLatchExample2 {
+    public static void main(String[] args) {
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Do some initial working......");
+                try {
+                    Thread.sleep(1000);
+                    latch.await();
+                    System.out.println("Do other working......");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                System.out.println("asyn perpare for some data.");
+                try {
+                    Thread.sleep(2000);
+                    System.out.println("data prepare for done.");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            }
+        }.start();
+    }
+}
---
+++ src/main/java/com/zhangj/ymm/thread/juc/utils/CountDownLatchExample3.java
@@ -0,0 +1,11 @@
+package com.zhangj.ymm.thread.juc.utils;
+
+/**
+ * @author zhangj
+ * @date 2019/4/8
+ */
+public class CountDownLatchExample3 {
+    public static void main(String[] args) {
+
+    }
+}
