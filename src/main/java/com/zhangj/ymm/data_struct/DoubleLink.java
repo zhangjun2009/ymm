0532328bd563967e619d5966af6b581e3a9bc710
zhangj <814674562@qq.com>
2019-06-09 11:52:56 +0800

    objects test

---
+++ src/main/java/com/zhangj/ymm/data_struct/DoubleLink.java
@@ -0,0 +1,139 @@
+package com.zhangj.ymm.data_struct;
+
+/**
+ * @author zhangj
+ * @date 2019/6/9
+ * 双向链表的java实现
+ */
+public class DoubleLink<T> {
+    /**
+     * 链表的头
+     */
+    public Node<T> head;
+    /**
+     * 链表节点的个数
+     */
+    public int count;
+
+    public DoubleLink() {
+        /**
+         * 创建表头,表头没有数据
+         */
+        head = new Node<>(null, null, null);
+        /**
+         * 初始节点个数为0
+         */
+        count = 0;
+    }
+
+    /**
+     * 返回节点的数量
+     *
+     * @return
+     */
+    public int size() {
+        return count;
+    }
+
+    /**
+     * 判断链表是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    /**
+     * 获取index位置处的节点
+     *
+     * @param index 位置
+     * @return
+     */
+    public Node<T> getNode(int index) {
+        if (index < 0 || index >= count) {
+            throw new IndexOutOfBoundsException();
+        }
+        /**
+         * 正向查找
+         */
+        if (index <= count / 2) {
+            Node<T> nextNode = head.next;
+            for (int i = 0; i < index; i++) {
+                nextNode = nextNode.next;
+            }
+            return nextNode;
+        }
+        /**
+         * 反向查找
+         */
+        Node<T> prevNode = head.prev;
+        for (int j = 0; j < count - index - 1; j++) {
+            prevNode = prevNode.prev;
+        }
+        return prevNode;
+    }
+
+    /**
+     * 获取index位置节点的值
+     * * @param index
+     *
+     * @return
+     */
+    public T get(int index) {
+        return getNode(index).value;
+    }
+
+    /**
+     * 获取第一个节点的值
+     *
+     * @return
+     */
+    public T getFirst() {
+        return getNode(0).value;
+    }
+
+    /**
+     * 获取最后一个节点的值
+     *
+     * @return
+     */
+    public T getLast() {
+        return getNode(count - 1).value;
+    }
+
+    /**
+     * 将节点插入到第index位置之前
+     *
+     * @param index 位置
+     * @param t     值
+     */
+    public void insert(int index, T t) {
+        if (index == 0) {
+            Node<T> node = new Node<>(t, head, head.next);
+            head.next.prev = node;
+            head.next = node;
+            count++;
+            return;
+        }
+        Node<T> node = getNode(index);
+        Node<T> tNode = new Node<>(t, node.prev, node);
+        node.prev.next = tNode;
+        node.next=node;
+        count++;
+    }
+
+
+
+    public class Node<T> {
+        public T value;
+        public Node prev;
+        public Node next;
+
+        public Node(T value, Node prev, Node next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+}
