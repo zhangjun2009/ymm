230dbb84d0aba288030b42f77921b1bfe0db2f8f
zhangj <814674562@qq.com>
2019-06-20 11:15:00 +0800

    添加数组Java实现

--- src/main/java/com/zhangj/ymm/data_struct/array/GenericArray.java
+++ src/main/java/com/zhangj/ymm/data_struct/array/GenericArray.java
@@ -106,6 +106,112 @@ public class GenericArray<T> {
         return -1;
     }
 
+    /**
+     * 在index位置插入元素e
+     *
+     * @param index
+     * @param e
+     */
+    public void add(int index, T e) {
+        checkIndex(index);
+        //如果当前数组长度等于数组初始大小,则进行扩容
+        if (data.length == size) {
+            resize(2 * size);
+        }
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    /**
+     * 向数组头插入元素e
+     *
+     * @param e
+     */
+    public void addFirst(T e) {
+        add(0, e);
+    }
+
+    /**
+     * 向数组尾部插入元素e
+     *
+     * @param e
+     */
+    public void addLast(T e) {
+        add(size, e);
+    }
+
+    /**
+     * 删除index位置处的元素,并返回
+     *
+     * @param index
+     * @return
+     */
+    public T remove(int index) {
+        checkIndex(index);
+        T removeData = data[index];
+        for (int i = index; i < size; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+        data[index] = null;
+        return removeData;
+    }
+
+    /**
+     * 删除第一个元素
+     */
+    public void removeFirst() {
+        remove(0);
+    }
+
+    /**
+     * 删除最后一个元素
+     */
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    /**
+     * 从数组中删除
+     *
+     * @param e
+     */
+    public void removeElement(T e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        //builder.append(String.format("Array size = %d, capacity = %d \n", size, data.length));
+        builder.append('[');
+        for (int i = 0; i < size; i++) {
+            builder.append(data[i]);
+            if (i != size - 1) {
+                builder.append(", ");
+            }
+        }
+        builder.append(']');
+        return builder.toString();
+    }
+
+    /**
+     * 扩容
+     *
+     * @param capacity
+     */
+    private void resize(int capacity) {
+        T[] newData = (T[]) new Object[capacity];
+        System.arraycopy(data, 0, newData, 0, size);
+        data = newData;
+    }
+
     private void checkIndex(int index) {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Add failed! Require index >=0 and index <= size.");
@@ -116,5 +222,19 @@ public class GenericArray<T> {
         if (index < 0 || index >= size) {
             throw new IllegalArgumentException("remove failed! Require index >=0 and index < size.");
         }
+    }
+
+    public static void main(String[] args) {
+        GenericArray<Integer> array = new GenericArray<>(5);
+        array.add(0, 0);
+        array.add(1, 1);
+        array.add(2, 2);
+        array.add(3, 3);
+        System.out.println(array.toString());
+        array.addFirst(12);
+        array.addLast(22);
+        System.out.println(array.toString());
+        array.removeElement(11);
+        System.out.println(array.toString());
     }
 }
